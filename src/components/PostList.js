04646d28e16fce38a499cd2e7206fedd553f8e16
Will Greenberg <greenberg.will@gmail.com>
2022-04-29 16:01:13 -0700

    add post and postlist components

---
+++ src/components/PostList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "./Post";
+
+function PostList(){
+  return(
+    <React.Fragment>
+      <Post />
+    </React.Fragment>
+  )
+}
+
+export default PostList;
