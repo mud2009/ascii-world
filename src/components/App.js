1175cea0f2a2a9e1ca56f1c10d25d3c08b652338
Will Greenberg <greenberg.will@gmail.com>
2022-04-29 15:51:52 -0700

    import components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Header from "./Header";
+import Navbar from "./Navbar";
+import FeedControl from "./FeedControl";
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar />
+      <Header /> */}
+      <FeedControl />
     </div>
   );
 }
