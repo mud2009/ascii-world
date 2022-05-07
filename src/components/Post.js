a092bd918f1f531954dd17175e671df0894504a9
Will Greenberg <greenberg.will@gmail.com>
2022-05-06 17:10:25 -0700

    display firestore info, display images uploaded

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { storage } from "../firebase"
 
-function Post(){
+function Post(props){
+  var pathReference = storage.ref(`images/${props.imageName}`);
+  pathReference.getDownloadURL().then((url) => {
+    var img = document.getElementById('myimg' + props.imageName);
+    img.setAttribute('src', url);
+  })
+  .catch((error) => {
+    switch (error.code) {
+      case 'storage/object-not-found':
+        break;
+      case 'storage/unauthorized':
+        break;
+      case 'storage/canceled':
+        break;  
+      case 'storage/unknown':
+        break;
+    }
+  });
+  
   return(
     <React.Fragment>
-
+      <img id={`myimg${props.imageName}`} alt={`${props.imageName}`}/>
+      <h3>Name: {props.imageName} - Time: {props.timestamp}</h3>
     </React.Fragment>
   )
+}
+
+Post.propTypes = {
+  imageName: PropTypes.string,
+  timestamp: PropTypes.string
 }
 
 export default Post;
