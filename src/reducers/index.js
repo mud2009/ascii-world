cc8c189923b2858c142a1185747800373336459c
Will Greenberg <greenberg.will@gmail.com>
2022-05-06 17:08:14 -0700

    fix firestorereducer import statement

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import postListReducer from "./post-list-reducer";
 import { combineReducers } from "redux";
-import { firestoreReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
 
 const rootReducer = combineReducers({
   mainPostList: postListReducer,
