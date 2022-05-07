f75c328dddbd94e04485dc4890813cc8c1f82dbb
Will Greenberg <greenberg.will@gmail.com>
2022-05-06 17:45:03 -0700

    add header to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
 import React from "react";
-// import Header from "./Header";
+import Header from "./Header";
 // import Navbar from "./Navbar";
 import FeedControl from "./FeedControl";
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <Navbar />
-      <Header /> */}
+    <div className='main-container'>
+      {/* <Navbar /> */}
+      <Header />
       <FeedControl />
     </div>
   );
