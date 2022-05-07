8726c4122e9eecf99b3910c84391b8a20d79e8cc
Will Greenberg <greenberg.will@gmail.com>
2022-05-06 17:09:32 -0700

    add useselector, usefirestoreconnect to access information from firestore and pass into post

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,13 +1,35 @@
 import React from "react";
 import Post from "./Post";
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded } from 'react-redux-firebase'
 
 function PostList(){
-  return(
-    <React.Fragment>
-      <div>HAHAHAHAHHAAHHA</div>
-      <Post />
-    </React.Fragment>
-  )
+  useFirestoreConnect([
+    { collection: 'posts' }
+  ]);
+
+  const posts = useSelector(state => state.firestore.ordered.posts)
+
+  if (isLoaded(posts)){
+    return(
+      <React.Fragment>
+        {posts.map(post => {
+          return <Post 
+            imageName = {post.imageName}
+            timestamp = {post.timestamp}
+            key={post.imageName}
+            />
+        })}
+      </React.Fragment>
+    )  
+  } else {
+    return(
+      <React.Fragment>
+        <div>Loading....</div>
+      </React.Fragment>
+    )  
+
+  }
 }
 
 export default PostList;
