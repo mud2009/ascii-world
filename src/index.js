0638ea9832bd1bc2aa766e2faf4dd6716d87f7ca
Will Greenberg <greenberg.will@gmail.com>
2022-05-13 09:58:06 -0700

    remove redux, add bootstrap from index.js

--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'redux';
-import rootReducer from './reducers/index';
-import { Provider } from 'react-redux';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import { createFirestoreInstance } from 'redux-firestore';
-import {firebase} from "./firebase";
+import "bootstrap/dist/css/bootstrap.min.css"
 
-const store = createStore(rootReducer);
-
-const rrfProps = {
-  firebase,
-  config: {
-      userProfile: "users"
-    },
-  dispatch: store.dispatch,
-  createFirestoreInstance
-}
-
-store.subscribe(() =>
-  console.log(store.getState())
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
-
-ReactDOM.render(
-  <Provider store={store}>
-    <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
-    </ReactReduxFirebaseProvider>
-  </Provider>,
-  document.getElementById('root')
-)
