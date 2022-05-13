c2e7cff3d1f5cf505eac26f17ead4b10f86abd47
Will Greenberg <greenberg.will@gmail.com>
2022-05-13 14:59:11 -0700

    initialize firebase functions

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -13,46 +13,6 @@ export default function UploadImage() {
     }
   }
 
-
-  
-  function convertToASCII(input){
-    const ASCIICharacters = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'.".split("")
-    const charLength = ASCIICharacters.length;
-    const interval = charLength / 256;
-
-    // const convertToGrey = async (colorImage) => {
-    //   const bwImg =  colorImage.gamma().greyscale();
-    //   return bwImg
-    // }
-    const resize = async (bw, newWidth = 500) => {
-      const blackAndWhite = await bw;
-      const size = await blackAndWhite.metadata();
-      const ratio = size.width / size.height;
-      const newHeight = parseInt(newWidth * ratio);
-      const resized = await blackAndWhite.resize(newWidth, newHeight, { fit: "outside" })
-      return resized;
-    }
-    const pixelToASCII = async img => {
-      var newImg = await img;
-      const pixels = await newImg.raw().toBuffer();
-      const characters = "";
-      pixels.foreach(pixel => {
-        characters = characters + ASCIICharacters[Math.floor(pixel * interval)]
-      })
-      return characters;
-    }
-    const main = async (newWidth = 500) => {
-      const newImgData = await pixelToASCII(resize(input));
-      const pixels = newImgData.length;
-      let ASCII = "";
-      for (let i = 0; i < pixels; i += newWidth){
-        let line = newImgData.split("").slice(i, i + newWidth);
-        ASCII = ASCII + "\n" + line;
-      }
-    }
-    main()
-  }
-
   function handleUpload(e) {
     e.preventDefault();
     const blah = convertToASCII(file)
