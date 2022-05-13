5899150762d6773a224b3e3af8957f070ae62d98
Will Greenberg <greenberg.will@gmail.com>
2022-05-13 09:57:11 -0700

    add auth to firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,9 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/storage"
 import "firebase/compat/firestore"
+import "firebase/compat/auth"
 
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
@@ -10,13 +11,12 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID 
-}
+})
 
-firebase.initializeApp(firebaseConfig);
-firebase.firestore();
 
-const storage = firebase.storage();
+const storage = app.storage();
+const auth = app.auth()
+const db = app.firestore();
 
-const db = firebase.firestore();
-
-export {firebase, storage, db};
+export {auth, storage, db};
+export default app
