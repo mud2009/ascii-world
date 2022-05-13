b14238f0d5999220546e7795057188aa7939c89e
Will Greenberg <greenberg.will@gmail.com>
2022-05-13 10:01:01 -0700

    add routing, login, signup, profile, to app.js, wrap in authprovider

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import Header from "./Header";
-// import Navbar from "./Navbar";
+import MyNavbar from "./MyNavbar";
 import FeedControl from "./FeedControl";
-
+import LogIn from "./auth/LogIn"
+import SignUp from "./auth/SignUp"
+import Profile from "./auth/Profile"
+import { Container } from "react-bootstrap"
+import { AuthProvider } from "../contexts/AuthContext"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className='main-container'>
-      {/* <Navbar /> */}
-      <Header />
-      <FeedControl />
-    </div>
+    <>
+      <AuthProvider>
+      <MyNavbar />
+      <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "80vh" }}>
+        <div className="w-100" style={{maxWidth: '400px'}}>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<FeedControl/>}/>
+              <Route path="/signup" element={<SignUp/>}/>
+              <Route path="/login" element={<LogIn/>}/>
+              <Route path="/profile" element={<Profile/>}/>
+            </Routes>
+          </Router>
+        </div>
+      </Container>
+      </AuthProvider>
+    </>
   );
 }
 
