55eec7435d6ca5220e3581d9f43001114e013318
Will Greenberg <greenberg.will@gmail.com>
2022-05-15 20:09:52 -0700

    remove unused variables and hooks

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
-import {storage, db} from '../firebase';
+import { storage } from '../firebase';
 import { Button, Form } from 'react-bootstrap';
 
 export default function UploadImage() {
   const [file, setFile] = useState(null);
   const [buttonText, setButtonText ] = useState("Upload Image");
-  const timestamp = Date.now();
 
   function handleChange(e) {
     if (e.target.files[0]){
