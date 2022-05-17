45bc06b34ced67ff0c41fb51a9da60c5f3a91b39
Will Greenberg <greenberg.will@gmail.com>
2022-05-17 09:07:30 -0700

    add update profile route to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import FeedControl from "./FeedControl";
 import LogIn from "./auth/LogIn"
 import SignUp from "./auth/SignUp"
 import Profile from "./auth/Profile"
+import UpdateProfile from "./auth/UpdateProfile"
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
@@ -21,6 +22,7 @@ function App() {
               <Route path="/signup" element={<SignUp/>}/>
               <Route path="/login" element={<LogIn/>}/>
               <Route path="/profile" element={<Profile/>}/>
+              <Route path="/update-profile" element={<UpdateProfile/>}/>
             </Routes>
           </Router>
         </div>
