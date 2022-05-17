dc82480c9c9e515681b138fc8a9b75d997b3693f
Will Greenberg <greenberg.will@gmail.com>
2022-05-17 11:28:24 -0700

    remove routing from app.js, import approutes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,8 @@
 import React from "react";
 import MyNavbar from "./MyNavbar";
-import FeedControl from "./FeedControl";
-import LogIn from "./auth/LogIn"
-import SignUp from "./auth/SignUp"
-import Profile from "./auth/Profile"
-import UpdateProfile from "./auth/UpdateProfile"
+import AppRoutes from "./AppRoutes";
 import { Container } from "react-bootstrap"
 import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
@@ -16,15 +11,7 @@ function App() {
       <MyNavbar />
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "80vh" }}>
         <div id="mainContainer" className="w-100" style={{maxWidth: '750px'}}>
-          <Router>
-            <Routes>
-              <Route exact path="/" element={<FeedControl/>}/>
-              <Route path="/signup" element={<SignUp/>}/>
-              <Route path="/login" element={<LogIn/>}/>
-              <Route path="/profile" element={<Profile/>}/>
-              <Route path="/update-profile" element={<UpdateProfile/>}/>
-            </Routes>
-          </Router>
+          <AppRoutes/>
         </div>
       </Container>
       </AuthProvider>
