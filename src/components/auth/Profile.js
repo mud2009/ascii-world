5e77243258b3fd91a6275a6862c6d060ef991351
Will Greenberg <greenberg.will@gmail.com>
2022-05-17 09:08:38 -0700

    add link to update profile component

--- src/components/auth/Profile.js
+++ src/components/auth/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Card, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../../contexts/AuthContext'
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function Profile() {
   const { currentUser, logOut } = useAuth();
@@ -23,8 +23,9 @@ export default function Profile() {
         <Card.Body>
           <h2 className="text-center mb-4">Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-          {/* <Link to="update-profile" className='btn btn-primary w-100 mt-3'>Update Profile</Link> */}
+          <p><strong>Email:</strong> {currentUser.email}</p>
+          <p><strong>Username:</strong> {currentUser.displayName}</p>
+          <Link to="../update-profile" className='btn btn-primary w-100 mt-3'>Update Profile</Link>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
