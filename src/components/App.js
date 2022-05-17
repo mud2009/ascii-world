f8079347f968edc90a86c35a7053a9a2c952329f
Will Greenberg <greenberg.will@gmail.com>
2022-05-17 14:10:56 -0700

    change maxwidth property on app.js

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App() {
       <AuthProvider>
       <MyNavbar />
       <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "80vh" }}>
-        <div id="mainContainer" className="w-100" style={{maxWidth: '750px'}}>
+        <div id="mainContainer" className="w-100" style={{maxWidth: '625px'}}>
           <AppRoutes/>
         </div>
       </Container>
