bf2ade1eafc1e8a347ff8f09c9fd48556001ae3f
Will Greenberg <greenberg.will@gmail.com>
2022-05-18 11:41:48 -0700

    remove image call from post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,34 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { storage } from "../firebase"
 import { Card } from "react-bootstrap"
 
 function Post(props){
-  // var pathReference = storage.ref(`images/${props.imageName}`);
-  // pathReference.getDownloadURL().then((url) => {
-  //   var img = document.getElementById('myimg' + props.imageName);
-  //   img.setAttribute('src', url);
-  // })
-  // .catch((error) => {
-  //   switch (error.code) {
-  //     case 'storage/object-not-found':
-  //       break;
-  //     case 'storage/unauthorized':
-  //       break;
-  //     case 'storage/canceled':
-  //       break;  
-  //     case 'storage/unknown':
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // });
   
   return(
     <Card className="image-post mb-3">
       <Card.Body>
         <h2 id="poster">{props.user}</h2>
-        {/* <img className="center-block" id={`myimg${props.imageName}`} alt={`${props.imageName}`}/> */}
         <pre>
           {props.asciiData}
         </pre>
