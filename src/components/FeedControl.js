2f6599738c17615e3fe4401792634d2ba32846da
Will Greenberg <greenberg.will@gmail.com>
2022-05-19 14:05:57 -0700

    allow users to view feed without logging in

--- src/components/FeedControl.js
+++ src/components/FeedControl.js
@@ -1,29 +1,8 @@
 import React from "react";
-import { Card } from "react-bootstrap";
-import { useAuth } from "../contexts/AuthContext";
-import { Link } from "react-router-dom";
 import PostList from "./PostList";
 
 export default function FeedControl() {
-  const { currentUser } = useAuth();
-  
-  if(!currentUser){
-    return (
-      <>
-        <Card>
-          <Card.Body>
-            <h2 className="text-center mb-4">Log in to view ASCII World</h2>
-            <Link className='btn btn-primary w-100 mt-3' to="/login">Log In</Link>
-          </Card.Body>
-        </Card>
-      </>
-    )
-
-  } else {
       return(
-        <React.Fragment>
           <PostList/>
-        </React.Fragment>
       )
-    }  
-  }
+    }
